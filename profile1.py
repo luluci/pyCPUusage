08ea66e8f2fd2122628501608e2f3915d214dc71
luluci <lunacied@gmail.com>
2021-08-04 00:01:11 +0900

    add: multi thread pattern

--- profile1.py
+++ profile1.py
@@ -4,16 +4,9 @@ sys.path.append(os.path.join(os.path.dirname(__file__), './'))
 
 from typing import List
 
-from process.process import Process, Priority, ProcessTime
+from process.process import Process, Priority, ProcessTime, TraceInfo
 from process.process_tracer import ProcessTracer
 from process.profiler import Profiler_plantuml
-
-pri = Priority
-time = ProcessTime
-us = ProcessTime.usec
-ms = ProcessTime.msec
-TASK = Process.task
-INTR = Process.interrupt
 
 def profiler_test(procs: List[Process]):
 	profiler = Profiler_plantuml(procs)
@@ -24,6 +17,66 @@ def profiler_test(procs: List[Process]):
 	#profiler.make_log(tracer)
 	#
 	#profiler.make_plantuml(tracer)
+
+
+
+import queue
+import time
+import concurrent.futures
+from typing import Callable
+import pathlib
+
+
+def profiler_test2(procs: List[Process]):
+	profiler = Profiler_plantuml(procs)
+	tracer = ProcessTracer(procs)
+	q_log = queue.Queue(1000)
+	tracing = True
+
+	def log_hdler(log: TraceInfo):
+		q_log.put(log)
+
+	def log_thread():
+		# ファイルオープン
+		p = pathlib.Path("./test.plantuml")
+		fp = p.open(mode="w", encoding='utf-8')
+		# ヘッダ書き出し
+		for line in profiler.pu_header:
+			fp.write(line + "\n")
+		# 解析ログ取得
+		while tracing:
+			if not q_log.empty():
+				log = q_log.get_nowait()
+				profiler.logging(log)
+				for line in profiler.pu_body:
+					fp.write(line + "\n")
+				profiler.pu_body = []
+			#time.sleep(0.001)
+		# フッタ書き出し
+		for line in profiler.pu_footer:
+			fp.write(line + "\n")
+
+	Process.set_log_callback(log_hdler)
+	executer = concurrent.futures.ThreadPoolExecutor(max_workers=1)
+	future_comm_hdle = executer.submit(log_thread)
+	#
+	try:
+		tracer.run()
+	except:
+		print("except!")
+	finally:
+		tracing = False
+	executer.shutdown()
+
+
+
+
+pri = Priority
+time = ProcessTime
+us = ProcessTime.usec
+ms = ProcessTime.msec
+TASK = Process.task
+INTR = Process.interrupt
 
 
 if False:
@@ -98,4 +151,4 @@ if True:
 		INTR('INTR1', pri(100, True), time(us(500), us(400))),
 	]
 	# 実行
-	profiler_test(procs)
+	profiler_test2(procs)
